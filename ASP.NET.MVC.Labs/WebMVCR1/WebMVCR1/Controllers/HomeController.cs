9668c48aecaa758e899f5b6ac486bc23ba328999
Maria Spirid <lan194@hotmail.com>
2021-09-23 19:47:05 +0300

    MVC Labs: Ex. 1 of Lab 1

---
+++ ASP.NET.MVC.Labs/WebMVCR1/WebMVCR1/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebMVCR1.Controllers
+{
+    public class MyController : Controller
+    {
+        // GET: Home
+        //public ActionResult Index()
+        //{
+        //    return View();
+        //}
+
+        public string Start() 
+        { 
+            int hour = DateTime.Now.Hour; 
+            string Greeting = hour < 12 ? "Top of the morning to ya!" : "Afternoon!"; 
+            return Greeting; 
+        }
+    }
+}
